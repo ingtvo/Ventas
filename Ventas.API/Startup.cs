2cf27314c51591a7193249711f74e795858cb94f
Ingtvo <ingtvo@msn.com>
2018-10-18 10:31:54 -0500

    Arquitectura completa con DB
    Listo para registrar

---
+++ Ventas.API/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ventas.API.Startup))]
+namespace Ventas.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
